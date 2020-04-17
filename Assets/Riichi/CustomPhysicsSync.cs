a85ccf2227be37695ae79e75ab4c34b43178bad8
Hiina Kuukan <hiina@hiina.space>
2020-04-16 19:37:18 -0600

    Commit full network limit probing setup
    since I'm going to tear it down afterwards. Has interesting
    comments with results though.

--- Assets/Riichi/CustomPhysicsSync.cs
+++ Assets/Riichi/CustomPhysicsSync.cs
@@ -1,9 +1,9 @@
 
-using Amazon.CognitoIdentity.Model;
 using System;
 using UdonSharp;
 using UnityEngine;
 using UnityEngine.Analytics;
+using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
 
@@ -15,157 +15,235 @@ public class CustomPhysicsSync : UdonSharpBehaviour
     // updates reasonably speedy, considering how slow udon execution is.
     // XXX UdonBehavior[] doesn't exist in udon types, so have to cast it dynamically
     public float syncIntervalSecs = 0.1f;
+    public Slider syncIntervalSlider;
+    public Text syncIntervalText;
+    public Slider chunkSizeSlider;
+    public Text chunkSizeText;
+    public Toggle writeFullRandom;
 
     private Component[] tiles;
     private float lastSync = 0;
 
-    public int tilesPerStripe = 4;
-    private int stripe;
-
-    private UInt16 seqNo = 0;
+    private int maxStringSize = 16;
+    private int seqNo = 0;
 
     // master will have full control now.
-    [UdonSynced] public string state;
+    [UdonSynced] public string a;
     // let's try this
-    [UdonSynced] public string state1;
-    [UdonSynced] public string state2;
-    [UdonSynced] public string state3;
+    [UdonSynced] public string b;
+    //[UdonSynced] public string state3;
 
     void Start()
     {
         // GetComponentsInChildren includes "this", annoying
-        var tilesAndThis = transform.GetComponentsInChildren(typeof(UdonBehaviour));
-        tiles = new Component[136];
-        for (int i = 0; i < 136; ++i)
-        {
-            tiles[i] = tilesAndThis[i + 1];
-        }
+        //var tilesAndThis = transform.GetComponentsInChildren(typeof(UdonBehaviour));
+        //tiles = new Component[136];
+        //for (int i = 0; i < 136; ++i)
+        //{
+        //    tiles[i] = tilesAndThis[i + 1];
+        //}
         //Debug.Log($"my player id: {VRCPlayerApi.GetPlayerId(Networking.LocalPlayer)}");
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        lastSync += Time.deltaTime;
-        if (lastSync > syncIntervalSecs)
-        {
-            lastSync = 0;
-            if (Networking.IsMaster)
+        if (Networking.IsMaster) {
+            lastSync += Time.deltaTime;
+            syncIntervalSecs = syncIntervalSlider.value / 1000; // in ms
+            syncIntervalText.text = $"{syncIntervalSecs}ms sync interval";
+            if (lastSync > syncIntervalSecs)
             {
-                // I think sending an event makes the assembly for FixedUpdate more efficient
-                // compared to actually calling the method. dunno for sure
-                SendCustomEvent("DoSync");
-            } else
-            {
-                SendCustomEvent("ReadSync");
+                lastSync = 0;
+                {
+                    // I think sending an event makes the assembly for FixedUpdate more efficient
+                    // compared to actually calling the method. dunno for sure
+                    //SendCustomEvent("DoSync");
+                    DoSync();
+                }
             }
+        }
+        else
+        {
+            ReadSync();//SendCustomEvent("ReadSync");
         }
     }
 
     public void DoSync()
     {
-        // 1 char idx into tiles array, 3 chars position, 2 chars rotation = 7 chars each tile;
-        // total of 136 * 7 = 952 chars of state.
-        // synced string has less than 56 chars, maybe it's 32?
-        // you can stripe across a single synced string to a degree; unknown how many synced strings
-        // are possible
-        state = new string(writePacket());
-        state1 = new string(writePacket());
-        state2 = new string(writePacket());
-        state3 = new string(writePacket());
+        // 1 char idx into tiles array, 2 chars position, 2 chars rotation = 6 chars each tile;
+        // total of 136 * 6 = 816 chars of state.
+
+        // testing the parameters of how many synced strings and the data you put in them
+        // and how many bytes you can fit until udon chokes.
+
+        // type   num strings    max str len    max bytes   
+        // short     1           42                 84 (seems possible at 43 chars, but only half the time)
+        // byte      1           126                126
+        // short     2           35                 140
+        // byte      2           105                210
+        // short     3           19                 108
+        // byte      3           55                 165
+        // short     4           12                 96
+        // byte      4           33                 132
+
+        // got interesting error 'Caught InsufficientMemoryException while serializing,
+        // encoded size is very large 238 >= 238'. So that's an interesting limit.
+        // seems to happen irregularly with random packets, so there's some sort of compression
+        // going on it seems.
+        
+        // for timing, the fastest you can change a string and expect them to sync is about 200ms.
+        // any lower and packets will get dropped; even 200ms isn't completely reliable and may be better
+        // on my network than most, but it seems _mostly_ reliable. packet loss seems the same no matter
+        // how many strings.
+
+        // on a whim I tried to change the synce variables to single-char variable names. doesn't affect 
+        // serialization or capacity though.
+
+        // goood news: the number of distinct UdonBehaviors with synced variables scales linearly at least to
+        // 5 though (with 2 each since that's the max bandwidth). Packet loss at 200ms is still near 0, and limits
+        // are the same. Going to keep duplicating gameobjects until I find the limit. I think it's under
+        // 136 given that broke with "death run detected".
+        // turns out the limit of UdonSynced objects is between 8 and 16, don't want to probe any further since
+        // 5 is enough for my purposes. Interestingly with 16 objects, the sync timing is still 200ms for 0% loss
+        // and seems to scale the same loss when you decrease the timing. The max per-object is all the same too.
+
+        // so the max reliable throughput for udon is with 8 objects with 2 synced strings each, using only bytes
+        // for 210 * 8 = 1680 bytes , at 200ms is  8400 Bps or 67.2kbps.
+
+        a = new string(writePacket());
+        b = new string(writePacket());
+        //state3 = new string(writePacket());
+       Debug.Log($"{gameObject.name} wrote packet {seqNo}");
+        seqNo = (ushort)(seqNo + 1);
     }
     char[] writePacket()
     {
-        // add a seqNo to the beginning, so 29 chars;
-        char[] pack = new char[29];
-        pack[0] = Convert.ToChar(seqNo++); // dumb work avoiding thing
-        int n = 1;
-        for (int j = 0; j < tilesPerStripe; ++j)
+        maxStringSize = (int)chunkSizeSlider.value;
+        chunkSizeText.text = $"{maxStringSize} chars";
+        // add a seqNo to the beginning
+        char[] pack = new char[maxStringSize];
+        pack[0] = Convert.ToChar(seqNo); // dumb work avoiding thing
+        int n = 1, i = 0;
+        // just write random data for now
+        if (writeFullRandom.isOn)
         {
-            pack[n] = Convert.ToChar(stripe);
-            n += 1;
-            packPosition(tiles[stripe].transform.position, pack, n);
-            n += 3;
-            packQuaternion(tiles[stripe].transform.rotation, pack, n);
-            n += 2;
-            // wrap around if we're done
-            stripe = (stripe + 1) % 136;
+            while (n < maxStringSize)
+            {
+                var p = UnityEngine.Random.Range(0, 65535);
+                pack[n++] = Convert.ToChar(p);
+            }
+        } else
+        {
+            while (n < maxStringSize)
+            {
+                pack[n++] = (char)UnityEngine.Random.Range(0,127);
+            }
         }
+        //while (n+5 < maxStringSize)
+        //{
+        //    pack[n] = (char)i;
+        //    n += 1;
+        //    packPosition(tiles[i].transform.position, pack, n);
+        //    n += 2;
+        //    packQuaternion(tiles[i].transform.rotation, pack, n);
+        //    n += 2;
+        //    // wrap around if we're done
+        //    i = (i + 1) % 136;
+        //}
+
         return pack;
     }
 
     public void ReadSync()
     {
-        readPacket(state);
-        readPacket(state1);
-        readPacket(state2);
-        readPacket(state3);
+        readPacket(a);
+       readPacket(b);
+        //readPacket(state3);
     }
 
     void readPacket(string pkt)
     {
         if (pkt.Length == 0) return;
         char[] pack = pkt.ToCharArray();
-        char seq = pack[0];
-        if (seqNo == seq)
-        {
-            // already read it
-            Debug.Log($"already read seq {seqNo}");
+        int seq = (int)pack[0];
+        if (seq != seqNo) {
+            Debug.Log($"{gameObject.name} read packet {seq}, packet loss of {seq - seqNo}");
         }
-        int n = 1;
-        for (int j = 0; j < tilesPerStripe; ++j)
-        {
-            int i = Convert.ToInt32(pack[n]);
-            n += 1;
-            var p = unpackPos(pack, n);
-            n += 3;
-            var r = unpackQuaternion(pack, n);
-            n += 2;
-            tiles[i].transform.position = p;
-            tiles[i].transform.rotation = r;
-        }
-        seqNo = seq; // ok we read it
-    }
-
-    string Pack(Transform t)
-    {
-        char[] bytes = new char[64];
-        byte a = (byte)(0 ^ 0xff & 0xaa << 3);
-        Convert.ToChar(a);
-        var thing = Convert.ToInt32(bytes[0]);
-        return new string(bytes);
+        //for (int j = 0; j < tilesPerStripe; ++j)
+        //{
+        //    int i = Convert.ToInt32(pack[n]);
+        //    n += 1;
+        //    var p = unpackPos(pack, n);
+        //    n += 2;
+        //    var r = unpackQuaternion(pack, n);
+        //    n += 2;
+        //    // TODO use localPosition, if there are mulitiple tables/move the table from 0,0
+        //    tiles[i].transform.position = p;
+        //    tiles[i].transform.rotation = r;
+        //}
+        seqNo = seq;
     }
 
     void packPosition(Vector3 p, char[] array, int idx)
     {
-        // shorts maybe overkill, oh well
+        // 11 bits for x and z between [1.2,1.2], 10 bits for y between [0.4, 0.9]
+        // covers the playable area of the table to millimeter precision
         var x = PackPosComponent(p.x);
-        var y = PackPosComponent(p.y);
+        var y = PackYComponent(p.y);
         var z = PackPosComponent(p.z);
-        array[idx] = Convert.ToChar(x);
-        array[idx+1] = Convert.ToChar(y);
-        array[idx+2] = Convert.ToChar(z);
+
+        uint pack = x;
+        pack = (pack << 11) + z;
+        pack = (pack << 10) + y;
+
+        //Debug.Log($"packed {p.x} {p.y} {p.z} into {x} {y} {z} into {pack}");
+
+        array[idx] = Convert.ToChar(pack >> 16);
+        array[idx+1] = Convert.ToChar(pack & 65535);
     }
     Vector3 unpackPos(char[] array, int idx)
     {
-        var ox = (uint)(array[idx]);
-        var oy = (uint)(array[idx + 1]);
-        var oz = (uint)(array[idx + 2]);
-        var x = UnpackPosComponent(ox);
-        var y = UnpackPosComponent(oy);
-        var z = UnpackPosComponent(oz);
+        uint pack = Convert.ToUInt32(array[idx]);
+        pack = (pack << 16) + Convert.ToUInt32(array[idx+1]);
+
+        uint a = (uint)((pack >> 21) & 2047);
+        uint b = (uint)((pack >> 10) & 2047);
+        uint c = (uint)(pack & 1023);
+
+        var x = UnpackPosComponent(a);
+        var z = UnpackPosComponent(b);
+        var y = UnpackYComponent(c);
+
+        //Debug.Log($"upacked {pack} into {a} {b} {c} into {x} {y} {z}");
+
         return new Vector3(x, y, z);
     }
 
     uint PackPosComponent(float f)
     {
-        // [-3, 3] to [0, 65535]
-        if (f < -3) f = -3;
-        if (f > 3) f = 3;
-        return Convert.ToUInt32(Mathf.Floor(0.5f * (f / 3 + 1f) * 65535f + 0.5f));
+        // [-1.2, 1.2] to [0, 2047]
+        if (f < -1.2f) f = -1.2f;
+        if (f > 1.2f) f = 1.2f;
+        double s = (0.5 * (f / 1.2 + 1) * 2047 + 0.5);
+        var r = Convert.ToUInt32(Mathf.Floor((float)s));
+        return r;
     }
-    float UnpackPosComponent(uint i)
+    float UnpackPosComponent(long i)
     {
-        return (i - 32768) * (1.0f / 65535f) * 6f;
+        return (i - 1024) * (1.0f / 2047f) * 2.4f;
+    }
+
+    uint PackYComponent(float f)
+    {
+        // [0.5, 0.9] to [0, 1023]
+        if (f < 0.5) f = 0.5f;
+        if (f > 0.9) f = 0.9f;
+        var r = Convert.ToUInt32(Mathf.Floor((float)((f - 0.5) / 0.4 * 1023 + 0.5)));
+        return r;
+    }
+    float UnpackYComponent(long i)
+    {
+        return i / 1023f * 0.4f + 0.5f;
     }
 
     void packQuaternion(Quaternion q, char[] array, int idx)
