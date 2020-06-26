f29e8aa037127d0b606b2201eaf22c93e39e4a2a
Hiina Kuukan <hiina@hiina.space>
2020-06-25 19:14:31 -0600

    Remove newly ignored files

--- Assets/Udon/Editor/ProgramSources/UdonGraph/UdonGraphProgramAssetEditor.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEditor;
-
-namespace VRC.Udon.Editor.ProgramSources
-{
-    [CustomEditor(typeof(UdonGraphProgramAsset))]
-    public class UdonGraphProgramAssetEditor : UdonAssemblyProgramAssetEditor
-    {
-    }
-}
